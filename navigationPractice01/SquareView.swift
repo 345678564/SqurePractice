36341f1f1d4966d9a63fbe1824aea0c939379161
345678564 <kimtaewan@gimtaewan-ui-MacBookPro.local>
2021-05-20 21:27:29 +0900

    WTF

---
+++ navigationPractice01/SquareView.swift
@@ -0,0 +1,40 @@
+//
+//  SquareView.swift
+//  navigationPractice01
+//
+//  Created by 김태완 on 2021/05/20.
+//
+
+import UIKit
+
+class SquareView: UIView {
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let context = UIGraphicsGetCurrentContext()!
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setLineWidth(1)
+        
+//        let line = 10
+        
+        for h in 0 ... 9 {
+            let horizontalOrigin = CGPoint(x: rect.minX, y: rect.minY + 1 / 10 * CGFloat(h) * rect.height)
+            let horizontalTo = CGPoint(x: rect.width, y: horizontalOrigin.y)
+            
+            context.move(to: horizontalOrigin)
+            context.addLine(to: horizontalTo)
+            
+            print("Origin : ", horizontalOrigin)
+            print("To : ", horizontalTo)
+
+        }
+        
+//        context.move(to: CGPoint(x: rect.minX, y: rect.minY))
+//        context.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+//
+//        context.move(to: CGPoint(x: 0, y: 0 + 1 / 10 * rect.height))
+//        context.addLine(to: CGPoint(x: rect.width, y: 0 + 1 / 10 * rect.height))
+        
+        context.strokePath()
+    }
+}
---
+++ navigationPractice01/SquareViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SquareViewController.swift
+//  navigationPractice01
+//
+//  Created by 김태완 on 2021/05/20.
+//
+
+import UIKit
+
+class SquareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
