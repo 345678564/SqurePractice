cc9f8111a22a3a25f91a75b5a4369c1e73db465d
345678564 <kimtaewan@gimtaewan-ui-MacBookPro.local>
2021-05-20 19:53:39 +0900

    initial set

---
+++ navigationPractice01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigationPractice01
+//
+//  Created by 김태완 on 2021/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
